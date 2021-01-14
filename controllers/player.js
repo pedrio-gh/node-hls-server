a325ede0307501bfe1afd1891c5fae92e24b4c91
Pedro Avila <pedroacuma@gmail.com>
2021-01-14 22:11:00 +0100

    more stuff

--- controllers/player.js
+++ controllers/player.js
@@ -1,5 +1,10 @@
+const { snakeCase } = require('lodash');
+
 const play = async (req, res) => {
-  const { channel } = req.query;
+  let { channel } = req.query;
+  channel = Buffer.from(channel, 'base64').toString('utf-8');
+  channel = snakeCase(channel);
+
   const source = `${process.env.HLS_SOURCE}/${channel}/index.m3u8`;
   res.render('player', { source });
 };
