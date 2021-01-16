888c669bc99f746f4049027022cd9e02c7a65e1b
Pedro Avila <pedroacuma@gmail.com>
2021-01-16 12:54:26 +0100

    Free channels support

--- controllers/player.js
+++ controllers/player.js
@@ -1,9 +1,17 @@
 const { snakeCase } = require('lodash');
 
+const FREE_CHANNELS = ['gol_television', 'cuatro'];
+const FREE_CHANNELS_URL = ['https://goltelevision.com/live', 'https://www.cuatro.com/en-directo/'];
+
 const play = async (req, res) => {
   let { channel } = req.query;
   channel = Buffer.from(channel, 'base64').toString('utf-8');
   channel = snakeCase(channel);
+
+  if (FREE_CHANNELS.includes(channel)) {
+    res.redirect();
+    return null;
+  }
 
   const source = `${process.env.HLS_SOURCE}/${channel}/index.m3u8`;
   res.render('player', { source });
