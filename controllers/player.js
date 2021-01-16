6e99880cbb7af7d0548ad468b4b7b978512836e0
Pedro Avila <pedroacuma@gmail.com>
2021-01-16 12:59:33 +0100

    Proper redirect to free channel

--- controllers/player.js
+++ controllers/player.js
@@ -1,15 +1,17 @@
 const { snakeCase } = require('lodash');
 
-const FREE_CHANNELS = ['gol_television', 'cuatro'];
-const FREE_CHANNELS_URL = ['https://goltelevision.com/live', 'https://www.cuatro.com/en-directo/'];
+const FREE_CHANNELS = {
+  gol_television: 'https://goltelevision.com/live',
+  cuatro: 'https://www.cuatro.com/en-directo/',
+};
 
 const play = async (req, res) => {
   let { channel } = req.query;
   channel = Buffer.from(channel, 'base64').toString('utf-8');
   channel = snakeCase(channel);
 
-  if (FREE_CHANNELS.includes(channel)) {
-    res.redirect();
+  if (Object.keys(FREE_CHANNELS).includes(channel)) {
+    res.redirect(FREE_CHANNELS[channel]);
     return null;
   }
 
