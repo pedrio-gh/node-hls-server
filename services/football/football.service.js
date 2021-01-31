cbd131a919fed5beb6a5d2c144bbf388ed95a91f
Pedro Avila <pedroacuma@gmail.com>
2021-01-31 16:23:05 +0100

    Better chanel name filtering

--- services/football/football.service.js
+++ services/football/football.service.js
@@ -1,3 +1,4 @@
+const { snakeCase } = require('lodash');
 const SportsApiClient = require('../../sportsdb_api/client');
 const TvScheduleAPI = require('../../tv_schedule_api');
 
@@ -6,6 +7,7 @@ const Utils = require('../../lib/utils');
 const LEAGUES = require('./leagues.json');
 
 const CHANNELS = ['MLIGA-CODE', 'MLIG1-CODE', 'MLIG2-CODE', 'MLIG3-CODE', 'MLIG4-CODE', 'MLIG5-CODE', 'MLIG6-CODE', 'MLIG7-CODE', 'MLIG8-CODE', 'MVF1-CODE', 'CHAPIO-CODE', 'CHAP1-CODE', 'CHAP2-CODE', 'CHAP3-CODE', 'CHAP4-CODE', 'CHAP5-CODE', 'CHAP6-CODE', 'CHAP7-CODE', 'CHAP8-CODE', 'M1SD-CODE', 'M2SD-CODE', 'VAMOSD-CODE', 'C4-CODE', 'GOL-CODE'];
+
 const client = new SportsApiClient({ apiKey: process.env.SPORTS_DB_API_KEY });
 
 let todayListingJSON;
@@ -14,7 +16,8 @@ function findEventChannels(schedule, event) {
   return Object.keys(schedule).find((ch) => CHANNELS.includes(ch)
   && schedule[ch].PROGRAMAS.find((pr) => pr.CODIGO_GENERO === 'DP'
   && pr.DIRECTO
-  && (pr.TITULO.includes(event.strHomeTeam) || pr.TITULO.includes(event.strAwayTeam))));
+  && (snakeCase(pr.TITULO).includes(snakeCase(event.strHomeTeam))
+  || snakeCase(pr.TITULO).includes(snakeCase(event.strAwayTeam)))));
 }
 
 const fetchTodayListing = async () => {
