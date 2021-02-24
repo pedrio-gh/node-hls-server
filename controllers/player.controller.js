0562c9f25087f6b916814dd75ab3259e1012d42a
Pedro Avila <pedroacuma@gmail.com>
2021-02-24 20:33:16 +0100

    Iptv with proxies

--- controllers/player.controller.js
+++ controllers/player.controller.js
@@ -4,7 +4,7 @@ const index = async (req, res) => {
   const { channel } = req.query;
 
   const hlsSession = getHlsSession(channel);
-  const source = `${process.env.HLS_SOURCE}/${channel}/index.m3u8`;
+  const source = `${hlsSession.url}/index.m3u8`;
   res.render('player', { source });
 };
 
--- services/iptv/iptv.service.js
+++ services/iptv/iptv.service.js
@@ -1,23 +1,50 @@
-const hlsServer = require('../../lib/hlsServer');
+const POST = require('bent')('json', 'POST');
 const iptvSwitcher = require('../../lib/iptv_switcher');
 
-hlsServer.run();
+iptvSwitcher.loadIptv(`${__dirname}/iptv_procaja_1.m3u8`);
 iptvSwitcher.loadIptv(`${__dirname}/iptv_gooi_12m1.m3u8`);
+iptvSwitcher.loadIptv(`${__dirname}/iptv_gooi_12m2.m3u8`);
+iptvSwitcher.loadIptv(`${__dirname}/iptv_gooi_12m3.m3u8`);
 
-const getHlsSession = (chName) => {
-  let session = hlsServer.hlsSessions.get(chName);
+const sessions = [];
+
+const proxies = [
+  { id: 'proxy1', url: 'http://75.119.133.53', active: false },
+  { id: 'proxy2', url: 'http://75.119.133.54', active: false },
+];
+
+const getHlsSession = async (chName) => {
+  const session = sessions.find((s) => s.id === chName);
 
   if (!session) {
     const iptv = iptvSwitcher.getAvailableIptv();
     const channel = iptv.getChannels().find((c) => c.inf.title === chName);
-    hlsServer.addStream(chName, channel.url);
-    session = hlsServer.hlsSessions.get(chName);
-    iptv.activate();
+
+    const proxy = proxies.find((p) => !p.active);
+
+    const response = await POST(`${proxy}/start`, {
+      id: chName,
+      sourceUrl: channel.url,
+    });
+
+    if (response.ok) {
+      proxy.active = true;
+      sessions.push({ id: chName, url: `${proxy.url}/${chName}/index.m3u8` });
+      iptv.activate();
+    } else {
+      throw new Error('Error starting proxy');
+    }
   }
 
   return session;
 };
 
+const deleteHlsSession = (chName) => {
+  const index = sessions.findIndex((session) => session.id === chName);
+  sessions.splice(index, 1);
+};
+
 module.exports = {
   getHlsSession,
+  deleteHlsSession,
 };
